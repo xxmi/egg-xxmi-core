931c8d99acbe58204b818a4e587a083db55aad8e
121343789@qq.com <www.xxmi.cn>
2018-12-05 10:08:13 +0800

    1. 权限配置

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -22,8 +22,8 @@ const _hasWhiteList = (whiteList, path) => {
 
 module.exports = (options, app) => {
   return async function auth (ctx, next) {
-    const {app: {config}, request, logger, session} = ctx;
-    const {auth: {whiteList}} = config;
+    const {app: {config}, request, logger} = ctx;
+    const {auth: {whiteList, unsafe, url}} = config.xxmiCore;
     const {path} = request;
     const isAuth = ctx.isAuthenticated(); // true 已经登录
     const hasWhite = _hasWhiteList(whiteList, path); // true 白名单
@@ -33,11 +33,12 @@ module.exports = (options, app) => {
     }
     const accept = ctx.accept;
     const headers = accept.headers;
+    // Ajax 请求
     if (ctx.acceptJSON || (headers['content-type'] && headers['content-type'] === 'application/json')) {
       ctx.body = {code: 401, msg: '未登录'};
       ctx.status = 401;
-    } else {
-      ctx.redirect('/login');
+      return;
     }
+    unsafe === true ? ctx.redirect(url) : ctx.unsafeRedirect(url);
   };
 };
--- config/config.default.js
+++ config/config.default.js
@@ -6,5 +6,13 @@
  * @property {String} SOME_KEY - some description
  */
 exports.xxmiCore = {
-
+  auth: {
+    whiteList: [
+      '/no-auth-api',
+      /^\/reset-password(\/.+)?/,
+      '/401', '/403', '/404', '/login', '/authSuccess', '/authFailed', '/logout', '/captcha'
+    ],
+    url: '/login',
+    safe: true // true: redirect 模式；false: unsafeRedirect 模式
+  }
 };
