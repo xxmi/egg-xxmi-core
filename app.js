6be8ca04c6bb97679670aca221ed9125dfda5408
121343789@qq.com <www.xxmi.cn>
2018-12-05 11:04:53 +0800

    1. 插件配置

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 module.exports = app => {
-  const middleware = ['access', 'auth', 'notFound', 'uploadFile'];
-  app.config.appMiddleware.push(...middleware);
+  const {xxmiCore} = app.config;
+  const middleware = [];
+  for (let key in xxmiCore) {
+    const {name, enable} = xxmiCore[key];
+    if (enable && name) {
+      middleware.push(name);
+    }
+  }
+  if (middleware.length > 0) {
+    app.config.appMiddleware.push(...middleware);
+  }
 };
--- config/config.default.js
+++ config/config.default.js
@@ -6,7 +6,13 @@
  * @property {String} SOME_KEY - some description
  */
 exports.xxmiCore = {
+  access: {
+    name: 'access',
+    enable: true // 是否开启这个中间件
+  },
   auth: {
+    name: 'auth',
+    enable: true,
     whiteList: [
       '/no-auth-api',
       /^\/reset-password(\/.+)?/,
@@ -15,7 +21,13 @@ exports.xxmiCore = {
     url: '/login',
     safe: true // true: redirect 模式；false: unsafeRedirect 模式
   },
+  notFound: {
+    name: 'notFound',
+    enable: true
+  },
   uploadFile: {
+    name: 'uploadFile',
+    enable: true,
     baseDir: true, // true：需要 path.join(ctx.baseDir)
     tempPath: 'upload/temp'
   }
